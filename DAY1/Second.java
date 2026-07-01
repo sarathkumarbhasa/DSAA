64c77e97be10ba7d268940f5b3345d647a78f7c8
Sarath Kumar Bhasa <bhasasarathkumar051@gmail.com>
2026-07-01 23:24:29 +0530

    Create Second.java

---
+++ DAY1/Second.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Second {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hi " + name);
+
+        sc.close();
+    }
+}
