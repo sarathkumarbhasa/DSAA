f968d186a80c1e35237c90c06f856136ffe72420
Sarath Kumar Bhasa <bhasasarathkumar051@gmail.com>
2026-07-02 22:09:11 +0530

    Update Seven.java

--- DAY2/Seven.java
+++ DAY2/Seven.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+class Seven {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter ur number: ");
+        int num=sc.nextInt();
+        if (num<0)
+        {
+            System.out.println("Negative");
+        }
+        else if (num>0)
+        {
+            System.out.println("Positive");
+        }
+        else
+        {
+            System.out.println("zero");
+        }
+        
+        sc.close();
+        
+        
+    }
+}
